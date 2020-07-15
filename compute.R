d317a670416bb93df29505bafaec6ef720c4256a
Enrique Lopez Magallon <enadol@yahoo.com>
2020-07-15 12:51:42 +0200

    Add files via upload

---
+++ compute.R
@@ -0,0 +1,13 @@
+sumaCD <- function(country){
+  listCD <- c()
+  
+  for(i in 1:length(confirmed$Country.Region)){
+    if(confirmed$Country.Region[i]==country){
+        element <- confirmed[ncol(confirmed)][i,]
+        listCD$Confirmed[length(listCD$Confirmed)+1] <- element
+            }
+  }
+  
+  return(sum(listCD$Confirmed))
+}
+
